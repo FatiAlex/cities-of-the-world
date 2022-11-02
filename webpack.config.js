2d4200421ab2b2dd4faa4bf49b06370211ac4e1a
Fatima Alexandra Santos Catanho <fati.ale.es@gmail.com>
2022-11-02 18:35:47 +0100

    build: added clean webpack plugin dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -31,5 +32,6 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
     }),
+      new CleanWebpackPlugin(),
   ]
 };
