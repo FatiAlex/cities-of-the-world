69bceedc2120628189b417ffd24eb98300528f43
Fatima Alexandra Santos Catanho <fati.ale.es@gmail.com>
2022-11-02 18:17:44 +0100

    build: added babel dependecies and webpack config files

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }]
+  }
+};
