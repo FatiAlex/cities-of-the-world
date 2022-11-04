cf2c5ddbc769c6541136585ac7c87f4d43b93c4b
Fatima Alexandra Santos Catanho <fati.ale.es@gmail.com>
2022-11-04 20:58:28 +0100

    build: fix assets in jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,11 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  setupFilesAfterEnv: ['<rootDir>/jest/jest-setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/config/jest/jest-setup.ts'],
+  transform: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/config/jest/file-transformer.ts',
+  },
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!**/node_modules/**',
